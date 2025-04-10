656873ac13835820129d57e12a79ff7aaf2a8844
YEX01 <vortex8934@gmail.com>
2025-04-10 20:27:23 +0530

    Update git.py

--- Opus/core/git.py
+++ Opus/core/git.py
@@ -30,7 +30,7 @@ def install_req(cmd: str) -> Tuple[str, str, int, int]:
 
 
 def git():
-    REPO_LINK = "https://github.com/J9VX/VX7"
+    REPO_LINK = "https://github.com/YEX01/VXP"
     if config.GIT_TOKEN:
         GIT_USERNAME = REPO_LINK.split("com/")[1].split("/")[0]
         TEMP_REPO = REPO_LINK.split("https://")[1]
